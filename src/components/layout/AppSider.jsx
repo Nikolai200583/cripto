a41405ae84ad7289eb918479d6573674fcec343a
Beznosov Nikolai <Bonum_trd@bk.ru>
2024-02-08 16:39:47 +0500

    new

---
+++ src/components/layout/AppSider.jsx
@@ -0,0 +1,11 @@
+import { Layout, Card, Statistic} from "antd";
+
+const siderStyle = {
+ padding: '1rem',
+};
+
+export default function AppSider() {
+  return (
+    <Layout.Sider width="25%" style={siderStyle}> 
+    <Card>
+     
