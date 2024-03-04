6b40388025fc8e89ac0cd63113374c1428d918a0
Beznosov Nikolai <Bonum_trd@bk.ru>
2024-03-04 16:36:44 +0500

    button

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -1,17 +1,63 @@
-import { Layout } from "antd";
+import { Layout, Select, Space, Button } from "antd";
 
 const headerStyle = {
   width: '100%',
   textAlign: "center",  
   height: 60,
   padding: "1rem",
-  fisplay: 'flex',
+  display: 'flex',
   justifyContent: 'space-between',
   alignItems: 'center',
-  background: 'white'
- 
-};
 
+};
+const handleChange = (value) => {
+  console.log(`selected ${value}`);
+};
+const options = [
+  {
+    label: 'China',
+    value: 'china',
+    emoji: '🇨🇳',
+    desc: 'China (中国)',
+  },
+  {
+    label: 'USA',
+    value: 'usa',
+    emoji: '🇺🇸',
+    desc: 'USA (美国)',
+  },
+  {
+    label: 'Japan',
+    value: 'japan',
+    emoji: '🇯🇵',
+    desc: 'Japan (日本)',
+  },
+  {
+    label: 'Korea',
+    value: 'korea',
+    emoji: '🇰🇷',
+    desc: 'Korea (韩国)',
+  },
+];
 export default function AppHeader() {
-  return <Layout.Header style={headerStyle}></Layout.Header>;
+  return <Layout.Header style={headerStyle}>
+    <Select
+    // mode="multiple"
+    style={{
+      width: '250px',
+    }}
+    value = "press / to open"
+    optionLabelProp="label"
+    options={options}
+    optionRender={(option) => (
+      <Space>
+        <span role="img" aria-label={option.data.label}>
+          {option.data.emoji}
+        </span>
+        {option.data.desc}
+      </Space>
+    )}
+  />
+  <Button type="primary">Add Asset</Button>
+  </Layout.Header>;
 }
